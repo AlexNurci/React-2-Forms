aaba8f8f894e9611ad1f20e5c1ab48a3119a00b4
Alex Nurci <alexnurci@gmail.com>
2025-06-25 14:38:37 -0400

    Added title, author and iamge

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import AddBook from "./components/AddBook";
 import BookList from "./components/BookList";
 
 const App = () => {
+  const [books, setBooks] = useState([]);
+  const [authors, setAuthors] = useState([]);
+  const [images, setImages] = useState([]);
+  const appendBook = (newBook) => {
+    console.log("newBook", newBook);
+    setBooks([...books, newBook]);
+  }
+  const appendAuthor = (newAuthor) => {
+    console.log("newAuthor", newAuthor);
+    setAuthors([...authors, newAuthor]);
+  }
+  const appendImage = (newImage) => {
+    console.log("newImage", newImage);
+    setImages([...images, newImage]);
+  }
   return (
     <div className="app">
       <h1 className="title">React Forms! 📝</h1>
-      <AddBook />
-      <BookList />
+      <AddBook appendBook={appendBook} appendAuthor={appendAuthor} appendImage={appendImage}/>
+      {books.map((book) => (
+        <li key={book}>{book}</li>
+      ))}
     </div>
   );
 };
--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 /**
  * A book should have the following fields:
@@ -13,8 +14,82 @@ import React from "react";
  * - isFavorite (boolean, default false)
  */
 
-const AddBook = () => {
-  return <div>AddBook</div>;
+const AddBook = ({ appendBook, appendAuthor, appendImage }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [image, setImage] = useState("");
+  const [titleErrors, setTitleErrors] = useState([]);
+  const [dirty, setDirty] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("title", title);
+    appendBook(title);
+    appendAuthor(author);
+    appendImage(image);
+    clearForm();
+  };
+
+  const handleTitleChange = (event) => {
+    setDirty(true);
+    setTitle(event.target.value);
+    if (title.length < 5) {
+      setTitleErrors(["title must have at least 5 characters"]);
+    } else {
+      setTitleErrors([]);
+    }
+  };
+
+  const handleAuthorChange = (event) => {
+    setDirty(true);
+    setAuthor(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    setDirty(true);
+    setImage(event.target.value);
+  }
+
+  const clearForm = () => {
+    setTitle("");
+    setAuthor("");
+    setImage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="newBook">
+      <input
+        name="title"
+        type="text"
+        required
+        placeholder="Title"
+        value={title}
+        onChange={handleTitleChange}
+      />
+      <input
+        name="author"
+        type="text"
+        required
+        placeholder="Author"
+        value={author}
+        onChange={handleAuthorChange}
+      />
+      <input
+        name="image"
+        type="file"
+        required
+        placeholder="Image"
+        value={image}
+        onChange={handleImageChange}
+      />
+      {titleErrors.map((error) => (
+        <p className="error" key={error}>
+          {error}
+        </p>
+      ))}
+      <button disabled={titleErrors.length > 0 || !dirty}>Create Book</button>
+    </form>
+  );
 };
 
 export default AddBook;
